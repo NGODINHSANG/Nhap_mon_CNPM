8240ce5cfa1a326488c1f220aa451343754e37aa
vudinhhieu20 <64395908+vudinhhieu20@users.noreply.github.com>
2020-12-02 11:26:49 +0700

    Update Quan_ly_thu_phi.sql

--- Quan_ly_thu_phi.sql
+++ Quan_ly_thu_phi.sql
@@ -1,9 +1,9 @@
 USE quan_ly_nhan_khau;
 
 CREATE TABLE `dot_thu` (
-	  `idDotThu` int(11) NOT NULL,
+	`idDotThu` int(11) NOT NULL,
     `tenDotThu` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-   	`loaiPhiThu` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+	`loaiPhiThu` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
     `ngayBatDauThu` date DEFAULT NULL,
     `ngayKetThucThu` date DEFAULT NULL,
     `soTienTrenMotNhanKhau`int(11) NOT NULL
@@ -11,8 +11,8 @@ CREATE TABLE `dot_thu` (
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `thong_tin_thu_phi` (
-	  `maDotThu` int(11) NOT NULL,
-    `maHoKhau` int(11) NOT NULL,
+	`idDotThu` int(11) NOT NULL,
+    `idHoKhau` int(11) NOT NULL,
     `soNhanKhau` int(11) DEFAULT NULL,
     `tongSoTien` int(11) DEFAULT NULL,
     `ngaythu` date DEFAULT NULL
@@ -22,11 +22,11 @@ ALTER TABLE `dot_thu`
 ADD PRIMARY KEY (`idDotThu`);
 
 ALTER TABLE `thong_tin_thu_phi`
-ADD PRIMARY KEY (`maDotThu`,`maHoKhau`);
+ADD PRIMARY KEY (`idDotThu`,`idHoKhau`);
 
 ALTER TABLE `thong_tin_thu_phi`
-ADD FOREIGN KEY (`maDotThu`) REFERENCES `dot_thu`(`idDotThu`),
-ADD FOREIGN KEY (`maHoKhau`) REFERENCES `ho_khau`(`ID`);
+ADD FOREIGN KEY (`idDotThu`) REFERENCES `dot_thu`(`idDotThu`),
+ADD FOREIGN KEY (`idHoKhau`) REFERENCES `ho_khau`(`ID`);
 
 
 DROP TABLE `dot_thu`;
