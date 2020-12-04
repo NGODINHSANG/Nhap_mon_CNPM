f8d64d2bf06db9a000539bb18a6983806c6cdfd4
NGODINHSANG <sanga1k30@gmail.com>
2020-12-04 23:30:53 +0700

    update

--- QuanLyNhanKhau/src/models/DotThuModel.java
+++ QuanLyNhanKhau/src/models/DotThuModel.java
@@ -5,10 +5,75 @@
  */
 package models;
 
+import java.util.Date;
 /**
  *
- * @author Dell
+ * @author SangND
  */
 public class DotThuModel {
+    private int ID;
+    private String tenDotThu;
+    private String loaiPhiThu;
+    private Date ngayBatDauThu;
+    private Date ngayKetThucThu;
+    private int soTienTrenMotNhanKhau;
+    private Date ngayTao;
     
+    public int getID(){
+        return ID;
+    }
+    
+    public void setID(int ID){
+        this.ID = ID;
+    }
+    
+    public String getTenDotThu(){
+        return tenDotThu;
+    }
+    
+    public void setTenDotThu(String tenDotThu){
+        this.tenDotThu = tenDotThu;
+    }
+    
+    public String getLoaiPhiThu(){
+        return loaiPhiThu;
+    }
+    
+    public void setLoaiPhiThu(String loaiPhiThu){
+        this.loaiPhiThu = loaiPhiThu;
+    }
+    
+    public Date getNgayBatDauThu(){
+        return ngayBatDauThu;
+    }
+    
+    public void setNgayBatDauThu(Date ngayBatDauThu){
+        this.ngayBatDauThu = ngayBatDauThu;
+    }
+    
+    public Date getNgayKetThucThu(){
+        return ngayKetThucThu;
+    }
+    
+    public void setNgayKetThucThu(Date ngayKetThucThu){
+        this.ngayKetThucThu = ngayKetThucThu;
+    }
+    
+    public int getSoTienTrenMotNhanKhau(){
+        return soTienTrenMotNhanKhau;
+    }
+    
+    public void setSoTienTrenMotNhanKhau(int soTienTrenMotNhanKhau){
+        this.soTienTrenMotNhanKhau = soTienTrenMotNhanKhau;
+    }
+    
+    public Date getNgayTao(){
+        return ngayTao;
+    }
+    
+    public void setNgayTao(Date ngayTao){
+        this.ngayTao = ngayTao;
+    }
 }
+
+
