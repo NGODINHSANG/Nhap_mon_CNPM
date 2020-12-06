a1d881855ad9cc10af48bea81f602db945a25ac3
Cuong-cuu <Cuongcuu59@gmail.com>
2020-12-06 16:29:01 +0700

    Chinh sua file TableModelThuPhi.java

--- QuanLyNhanKhau/src/utility/TableModelThuPhi.java
+++ QuanLyNhanKhau/src/utility/TableModelThuPhi.java
@@ -16,34 +16,7 @@ import models.ThongTinThuPhiModel;
  */
 public class TableModelThuPhi {
     //bang cho main frame
-    public DefaultTableModel setTableThuPhi(List<DotThuBean> listItem,String[] listColumn){
-        final int columns = listColumn.length;
-        DefaultTableModel dtm = new DefaultTableModel()  {
-            @Override
-            public boolean isCellEditable(int row, int column) {
-                return super.isCellEditable(row, column); //To change body of generated methods, choose Tools | Templates.
-            }
-            @Override
-            public Class<?> getColumnClass(int columnIndex) {
-                return columnIndex == 3 ? Boolean.class : String.class;
-            }
-        };
-        dtm.setColumnIdentifiers(listColumn);
-        Object[] obj;
-        obj = new Object[columns];
-        listItem.forEach((DotThuBean item) -> {
-            obj[0] = item.getDotThuModel().getID();
-            obj[1] = item.getDotThuModel().getTenDotThu();
-            //obj[2] = item.getDotThuModel().getNgayBatDauThu().toString();
-            //obj[3] = item.getDotThuModel().getNgayKetThucThu().toString();
-            obj[2] = item.getDotThuModel().getSoTienTrenMotNhanKhau();
-            dtm.addRow(obj);
-        });
-        return dtm;
-    }
-    
-    //bang thong tin dot thu 
-    public DefaultTableModel setTableDotThu(List<ThongTinThuPhiModel> thongTin,String[] listColumn){
+    public DefaultTableModel setTableThuPhi(List<DotThuModel> listItem,String[] listColumn){
         final int columns = listColumn.length;
         DefaultTableModel dtm = new DefaultTableModel()  {
             @Override
@@ -58,11 +31,38 @@ public class TableModelThuPhi {
         dtm.setColumnIdentifiers(listColumn);
         Object[] obj;
         obj = new Object[columns];
-        thongTin.forEach((ThongTinThuPhiModel item) -> {
-            obj[0] = item.getIDHoKhau();
-            obj[1] = item.getSoNhanKhau();
-            obj[2] = item.getTongSoTien();
-            obj[3] = item.getNgayThu().toString();
+        listItem.forEach((DotThuModel item) -> {
+            obj[0] = item.getTenDotThu();
+            obj[1] = item.getLoaiPhiThu();
+            obj[2] = item.getNgayBatDauThu().toString();
+            obj[3] = item.getNgayKetThucThu().toString();
+            obj[4] = item.getSoTienTrenMotNhanKhau();
+            dtm.addRow(obj);
+        });
+        return dtm;
+    }
+    
+    //bang thong tin dot thu 
+    public DefaultTableModel setTableDotThu(List<ThongTinThuPhiBean> thongTin,String[] listColumn){
+        final int columns = listColumn.length;
+        DefaultTableModel dtm = new DefaultTableModel()  {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return super.isCellEditable(row, column); //To change body of generated methods, choose Tools | Templates.
+            }
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {
+                return columnIndex == 4 ? Boolean.class : String.class;
+            }
+        };
+        dtm.setColumnIdentifiers(listColumn);
+        Object[] obj;
+        obj = new Object[columns];
+        thongTin.forEach((ThongTinThuPhiBean item) -> {
+            obj[0] = item.getHoKhauModel().getMaHoKhau();
+            obj[1] = item.getThongTinThuPhiModel().getSoNhanKhau();
+            obj[2] = item.getThongTinThuPhiModel().getTongSoTien();
+            obj[3] = item.getThongTinThuPhiModel().getNgayThu().toString();
             dtm.addRow(obj);
         });
         return dtm;
