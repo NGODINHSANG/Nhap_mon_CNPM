c1258eaa7798e9178dbbb0c380d83f077f1e3099
anh-nnd183475 <anh.nnd183475@sis.hust.edu.vn>
2020-12-13 15:56:46 +0700

    fix controller Chi tiet thu phi

--- QuanLyNhanKhau/src/controllers/ThuPhiManagerController/ThongKeChiTietController.java
+++ QuanLyNhanKhau/src/controllers/ThuPhiManagerController/ThongKeChiTietController.java
@@ -5,15 +5,35 @@
  */
 package controllers.ThuPhiManagerController;
 
+import Bean.DotThuBean;
 import Bean.ThongTinThuPhiBean;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.EventObject;
+import java.util.List;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.table.DefaultTableModel;
+import models.DotThuModel;
 import services.MysqlConnection;
 import models.ThongTinThuPhiModel;
+import services.ThuPhiService;
+import utility.TableModelThuPhi;
+import views.infoViews.InfoJframe;
 
 /**
  *
@@ -21,12 +41,30 @@ import models.ThongTinThuPhiModel;
  */
 public class ThongKeChiTietController {
 
-    public boolean checkBtn(int maHKjtf) {
+    private List<ThongTinThuPhiBean> listThongTinThuPhiBean;
+    private ThuPhiService thuPhiService ;
+    private TableModelThuPhi tableModelThuPhi;
+    private final String COLUNMS[] = {"Mã đợt thu","Mã hộ khẩu", "Số nhân khẩu","Số tiền nộp","Ngày thu"}; 
+    private JPanel jpnViews;
+private JTextField jtfSearch;
+    private JTextField maHKjtf;
+    
+    public ThongKeChiTietController(JPanel panel, JTextField maHKjtfF ){
+        this.thuPhiService = new ThuPhiService();
+        this.listThongTinThuPhiBean = new ArrayList<>();
+        this.listThongTinThuPhiBean = thuPhiService.getListThongTinThuPhi();
+        this.tableModelThuPhi = new TableModelThuPhi();
+        this.maHKjtf = maHKjtfF;
+        this.jpnViews = panel;
+        this.jtfSearch= jtfSearch;
+      
+    }
+    public boolean checkBtn(String maHKjtf) {
         try{
             Connection connection = MysqlConnection.getMysqlConnection();
-            String query = "SELECT * FROM ho_khau WHERE ID = ?";
+            String query = "SELECT * FROM thong_tin_thu_phi WHERE maHoKhau = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1,maHKjtf);
+            preparedStatement.setString(1,maHKjtf);
             ResultSet rs = preparedStatement.executeQuery();
             if(rs.next()){
                 return true;
@@ -38,4 +76,59 @@ public class ThongKeChiTietController {
         return false;
     }
     
-}
+   
+    public void setData(String maHK){
+        //String  = null;
+       // try{
+       // if (this.maHKjtf.getText().trim().isEmpty()) {   
+      //      maHK= maHKjtf.getText();
+      //  }           
+       // }catch (Exception e) {
+       //     JOptionPane.showMessageDialog(maHKjtf, "Vui lòng nhập đúng kiểu dữ liệu!!", "Warring", JOptionPane.ERROR_MESSAGE);
+       // }        
+        this.listThongTinThuPhiBean = this.thuPhiService.getListThongTinThuPhi(maHK);
+                 
+        setDataTable();
+    
+    
+    }
+    public void setDataTable() {
+        List<ThongTinThuPhiModel> listItem = new ArrayList<>();
+        this.listThongTinThuPhiBean.forEach(thongTinThuPhi -> {
+            listItem.add(thongTinThuPhi.getThongTinThuPhiModel());
+        });
+        DefaultTableModel model = tableModelThuPhi.setTablethongtin(listItem, COLUNMS);
+        
+         
+         JTable table = new JTable(model) {
+            @Override
+            public boolean editCellAt(int row, int column, EventObject e) {
+                return false;
+            }   
+         };
+        
+        table.getTableHeader().setFont(new Font("Arial", Font.BOLD, 14));
+        table.getTableHeader().setPreferredSize(new Dimension(100, 50));
+        table.setRowHeight(50);
+        table.validate();
+        table.repaint();
+        table.setFont(new Font("Arial", Font.PLAIN, 14));
+      
+        
+        JScrollPane scroll = new JScrollPane();
+        scroll.getViewport().add(table);
+        scroll.setPreferredSize(new Dimension(1350, 400));
+        jpnViews.removeAll();
+        jpnViews.setLayout(new BorderLayout());
+        jpnViews.add(scroll);
+        jpnViews.validate();
+        jpnViews.repaint();
+    }
+
+    public JTextField getmaHKjtf(){
+        return maHKjtf;
+    }
+    public void setmaHKjtf(JTextField maHKjtf){
+        this.maHKjtf = maHKjtf;
+    }
+};
