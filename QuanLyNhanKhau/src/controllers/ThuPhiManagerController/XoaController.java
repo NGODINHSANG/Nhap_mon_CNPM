32cf952a166233cb6cd599e80bef1bff1d669c60
hieu <hieuvudinh409@gmail.com>
2020-12-14 19:48:31 +0700

    cập nhật thư viện chức năng

--- QuanLyNhanKhau/src/controllers/ThuPhiManagerController/XoaController.java
+++ QuanLyNhanKhau/src/controllers/ThuPhiManagerController/XoaController.java
@@ -23,12 +23,16 @@ public class XoaController {
         DotThuModel dotThuModel = dotThuBean.getDotThuModel();
         try{
             Connection connection = MysqlConnection.getMysqlConnection();
-            String query = "DELETE FROM thong_tin_thu_phi WHERE maDotThu = ? ;"
-                         + "DELETE FROM dot_thu WHERE maDotThu = ?;";
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, dotThuModel.getMaDotThu());
-            preparedStatement.execute();
-            preparedStatement.close();
+            String query1 = "DELETE FROM thong_tin_thu_phi WHERE maDotThu = ? ;";
+            String query2 = "DELETE FROM dot_thu WHERE maDotThu = ?;";
+            PreparedStatement preparedStatement1 = connection.prepareStatement(query1);
+            preparedStatement1.setString(1, dotThuModel.getMaDotThu());
+            PreparedStatement preparedStatement2 = connection.prepareStatement(query2);
+            preparedStatement2.setString(1, dotThuModel.getMaDotThu());
+            preparedStatement1.execute();
+            preparedStatement2.execute();
+            preparedStatement1.close();
+            preparedStatement2.close();
             connection.close();
             return true;
         }
