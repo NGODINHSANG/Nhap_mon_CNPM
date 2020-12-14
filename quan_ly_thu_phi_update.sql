5d626eb3bbf60e57f4eae0c77b66bcf3dd62c2aa
hieu <hieuvudinh409@gmail.com>
2020-12-14 19:40:26 +0700

    cập nhật hoàn thiện ver 2

--- quan_ly_thu_phi_update.sql
+++ quan_ly_thu_phi_update.sql
@@ -3,7 +3,7 @@ drop table if exists thong_tin_thu_phi;
 drop table if exists dot_thu;
 
 CREATE TABLE `dot_thu` (
-	`idDotThu` int(11) NOT NULL AUTO_INCREMENT,
+	`idDotThu` int(11) NOT NULL ,
     `maDotThu` varchar(10) DEFAULT NULL,
     `tenDotThu` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
 	`loaiPhiThu` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -23,13 +23,15 @@ CREATE TABLE `thong_tin_thu_phi` (
 
 ALTER TABLE `dot_thu`
 	ADD FULLTEXT KEY maDotThu (maDotThu),
-    ADD PRIMARY KEY idDotThu (idDotThu), idDotThu AUTO_INCREMENT=1;
+    ADD PRIMARY KEY idDotThu (idDotThu);
+ALTER TABLE `dot_thu` MODIFY COLUMN `idDotThu` INT AUTO_INCREMENT;
+ALTER TABLE `dot_thu` AUTO_INCREMENT=1;
 ALTER TABLE `thong_tin_thu_phi` 
  ADD PRIMARY KEY (maDotThu, maHoKhau);
--------------------------------------------------------------
+/*********************************************************/
 INSERT INTO `dot_thu` ( `maDotThu`, `tenDotThu`, `loaiPhiThu`, `ngayBatDauThu`, `ngayKetThucThu`, `soTienTrenMotNhanKhau`, `ngayTao`) VALUES ( 'DT001', 'Phí vệ sinh', 'Bắt buộc', '2020-12-08', '2020-12-15', '10000', '2020-12-07');
 INSERT INTO `dot_thu` ( `maDotThu`, `tenDotThu`, `loaiPhiThu`, `ngayBatDauThu`, `ngayKetThucThu`, `soTienTrenMotNhanKhau`, `ngayTao`) VALUES ( 'DT002', 'Quỹ khuyến học', 'Không bắt buộc', '2020-12-03', '2020-12-17', '0', '2020-12-01');
-
+/*********************************************************/
 INSERT IGNORE INTO `thong_tin_thu_phi` (maDotThu, maHoKhau,soNhanKhau)
 SELECT maDotThu, maHoKhau, soNhanKhau FROM
 (SELECT maDotThu FROM dot_thu ) dotthu 
